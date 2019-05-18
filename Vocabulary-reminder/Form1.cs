63e9da9005205254d978337460e64dd6bc2b6ec1
parham trojan <parhamtrojan@hotmail.com>
2019-05-19 00:33:39 +0430

    first commit

---
+++ Vocabulary-reminder/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vocabulary_reminder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width,
+                                   Screen.PrimaryScreen.WorkingArea.Height - this.Height);
+
+            try
+            {
+                
+                int counter = 0;
+                string line;
+                // Read the file and display it line by line.
+                System.IO.StreamReader file =
+                   new System.IO.StreamReader("words.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    counter++;
+                    label1.Text = line;
+                }
+                
+                file.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int counter = 0;
+                string line;
+                // Read the file and display it line by line.
+                System.IO.StreamReader file =
+                   new System.IO.StreamReader("words.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    counter++;
+                }
+                file.Close();
+
+                Random r = new Random();
+                int n = r.Next(counter);
+                // Read the file and display it line by line.
+                System.IO.StreamReader file1 =
+                   new System.IO.StreamReader("words.txt");
+                for (int i=0;i<n+1;i++)
+                {
+                    line = file1.ReadLine();
+                }
+                label1.Text = line;
+                file1.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2=new Form2();
+            f2.ShowDialog();
+
+        }
+    }
+}
---
+++ Vocabulary-reminder/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vocabulary_reminder
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText("words.txt", textBox1.Text);
+            MessageBox.Show("Your changes have been saved.");
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = System.IO.File.ReadAllText("words.txt");
+        }
+    }
+}
